8f53d5047c26fac4483f0937810aa03399753c65
Jaume Fullana <jfullana@cifpfbmoll.eu>
2021-02-28 17:10:56 +0100

    Afegits comentaris javaDoc.

--- src/test/java/com/cifpfbmoll/penjat3/PuntuacioTest.java
+++ src/test/java/com/cifpfbmoll/penjat3/PuntuacioTest.java
@@ -14,33 +14,58 @@ import org.junit.jupiter.api.BeforeEach;
  * @author jaume
  */
 public class PuntuacioTest {
-
+    /**
+     * Objecte de la clase Puntuacio per se inicialitzat abans de cada test.
+     */
     private Puntuacio puntuacio;
-
+    /**
+     * Metode que inicialitza la variable puntuacio. Aquest metode es crida 1 vegada
+     * antes de provar cada test.
+     */
     @BeforeEach
     public void reinici(){
         puntuacio=new Puntuacio();
     }
 
-
+    /**
+     * Metode test que corr el metode getParaulaSecretaDificultat damunt la instancia
+     * puntuacio, pasant per parametre un 1, i comprova si el intents que es guarden 
+     * en la instancia es igual a 5.
+     */
     @Test
     public void testGetParaulaSecretaDificultat1(){
         puntuacio.getParaulaSecretaDificultat(1);
         assertEquals(5,puntuacio.getIntents());
     }
-
+    /**
+     * Metode test que corr el metode getParaulaSecretaDificultat damunt la instancia
+     * puntuacio, pasant per parametre un 2, i comprova si el intents que es guarden 
+     * en la instancia es igual a 4.
+     */
     @Test
     public void testGetParaulaSecretaDificultat2() {
         puntuacio.getParaulaSecretaDificultat(2);
         assertEquals(4,puntuacio.getIntents());
     }
-
+    /**
+     * Metode test que corr el metode getParaulaSecretaDificultat damunt la instancia
+     * puntuacio, pasant per parametre un 7, i comprova si el intents que es guarden 
+     * en la instancia es igual a 0.
+     */
     @Test
     public void testGetParaulaSecretaDificultat3() {
         puntuacio.getParaulaSecretaDificultat(7);
         assertEquals(0,puntuacio.getIntents());
     }
-
+    /**
+     * Metode test que comprova si la funcio getParaulaSecretaDificultat retorna les
+     * paraules correctes. Primer crea un bolea igual false, despues crea una String paraula que rep
+     * el valor que retorna el metode getParaulaSecretaDificultat pasant-li per parametre
+     * un 1. Despres crea una Array de Strings que teoricament ha de contenir la praula
+     * que ha tornat la funcio getParaulaSecretaDificultat. Es comprova si es aixi, que
+     * la paraula que ha retornat esta alla dedins, a traves de un for que si la trova canvia
+     * el valor del boolea per true. Despres es comprova si el boolea es true.
+     */
     @Test
     public void testGetParaulaSecretaDificultat4() {
         boolean trobat=false;
@@ -53,7 +78,15 @@ public class PuntuacioTest {
         }
         assertTrue(trobat);
     }
-
+    /**
+     * Metode test que comprova si la funcio getParaulaSecretaDificultat retorna les
+     * paraules correctes. Primer crea un bolea igual false, despues crea una String paraula que rep
+     * el valor que retorna el metode getParaulaSecretaDificultat pasant-li per parametre
+     * un 3. Despres crea una Array de Strings que teoricament ha de contenir la praula
+     * que ha tornat la funcio getParaulaSecretaDificultat. Es comprova si es aixi, que
+     * la paraula que ha retornat esta alla dedins, a traves de un for que si la trova canvia
+     * el valor del boolea per true. Despres es comprova si el boolea es true.
+     */
     @Test
     public void testGetParaulaSecretaDificultat5() {
         boolean trobat=false;
@@ -66,7 +99,14 @@ public class PuntuacioTest {
         }
         assertTrue(trobat);
     }
-
+    /**
+     * Metode test que prova quants de punts es consegueixen amb una praulaEndevinada concreta.
+     * Primer crea una Array de Strings on el primer index es null, 
+     * crida al metode getParaulaSecretaDificultat pasant-li un 1 per parametre
+     * i despres crea un float que rep el valor que retorna la funcio calcularPuntuacio,
+     * que rep per parametre la Array de Strings i un int 3. Despres comprova si 
+     * el float es igual a 60.
+     */
     @Test
     public void testCalcularPuntuacio1() {
         String[] palabraEdivinada = {null,"a","r","a","u","l","a"};
@@ -74,8 +114,14 @@ public class PuntuacioTest {
         float puntuacioReal = puntuacio.calcularPuntuacio(palabraEdivinada,3);
         assertEquals(60,puntuacioReal);
     }
-    
-
+    /** 
+     * Metode test que prova quants de punts es consegueixen amb una praulaEndevinada concreta.
+     * Primer crea una Array de Strings on on hi ha varis index null, 
+     * crida al metode getParaulaSecretaDificultat pasant-li un 3 per parametre
+     * i despres crea un float que rep el valor que retorna la funcio calcularPuntuacio,
+     * que rep per parametre la Array de Strings i un int 3. Despres comprova si 
+     * el float es igual a 90.
+     */
     @Test
     public void testCalcularPuntuacio2() {
         String[] palabraEdivinada = {null,null,"r",null,"u","l",null};
@@ -83,13 +129,24 @@ public class PuntuacioTest {
         float puntuacioReal = puntuacio.calcularPuntuacio(palabraEdivinada,3);
         assertEquals(90,puntuacioReal);
     }
-//nvl 1=200, 2=400, 3=600 si no hi ha paraula bonus, si hi ha +10
+    /** 
+     * Metode test que prova quants de punts es consegueixen amb una praulaEndevinada concreta.
+     * Primer crea una String paraula que rep el valor de la funcio getParaulaSecretaDificultat
+     * pasantli per parametre un int 1. Crea una Array de Strings palabraEndevinada amb una
+     * llaragaria igual a la String paraula creada antes, despres assigna a cada index de 
+     * palabraEndevinada el caracter d'aquell mateix index en la String paraula, a traves de un for.
+     * Despres es crea un int anomenat puntuacioEsperada que es igual a 210 si la String paraula conte 
+     * els caracters: "x", "y" o "h" o a 200 si no conte cap d'aquests caracters. Despres es crea un 
+     * float puntuacioReal que rep el valor que retorna la funcio calcularPuntuacio, que rep per 
+     * parametre la Array de Strings palabraEndevinada i un int 3. Despres comprova si 
+     * puntuacioEsperada es igual a puntuacioReal.
+     */
     @Test
     public void testCalcularPuntuacio3() {
         String paraula=puntuacio.getParaulaSecretaDificultat(1);
         String [] palabraEndevinada = new String[paraula.length()];
         for (int n = 0; n < paraula.length(); n++) {
-                palabraEndevinada[n] =String.valueOf(paraula.charAt(n)) ;
+                palabraEndevinada[n]=String.valueOf(paraula.charAt(n)) ;
         }
         int puntuacioEsperada;
         if (paraula.contains("x") || paraula.contains("y") || paraula.contains("h")){
@@ -101,8 +158,18 @@ public class PuntuacioTest {
         float puntuacioReal = puntuacio.calcularPuntuacio(palabraEndevinada,3);
         assertEquals(puntuacioEsperada,puntuacioReal);
     }
-
-
+    /** 
+     * Metode test que prova quants de punts es consegueixen amb una praulaEndevinada concreta.
+     * Primer crea una String paraula que rep el valor de la funcio getParaulaSecretaDificultat
+     * pasantli per parametre un int 2. Crea una Array de Strings palabraEndevinada amb una
+     * llaragaria igual a la String paraula creada antes, despres assigna a cada index de 
+     * palabraEndevinada el caracter d'aquell mateix index en la String paraula, a traves de un for.
+     * Despres es crea un int anomenat puntuacioEsperada que es igual a 410 si la String paraula conte 
+     * els caracters: "x", "y" o "h" o a 400 si no conte cap d'aquests caracters. Despres es crea un 
+     * float puntuacioReal que rep el valor que retorna la funcio calcularPuntuacio, que rep per 
+     * parametre la Array de Strings palabraEndevinada i un int 3. Despres comprova si 
+     * puntuacioEsperada es igual a puntuacioReal.
+     */
     @Test
     public void testCalcularPuntuacio4() {
         String paraula=puntuacio.getParaulaSecretaDificultat(2);
@@ -120,7 +187,18 @@ public class PuntuacioTest {
         float puntuacioReal = puntuacio.calcularPuntuacio(palabraEndevinada,3);
         assertEquals(puntuacioEsperada,puntuacioReal);
     }
-
+    /** 
+     * Metode test que prova quants de punts es consegueixen amb una praulaEndevinada concreta.
+     * Primer crea una String paraula que rep el valor de la funcio getParaulaSecretaDificultat
+     * pasantli per parametre un int 3. Crea una Array de Strings palabraEndevinada amb una
+     * llaragaria igual a la String paraula creada antes, despres assigna a cada index de 
+     * palabraEndevinada el caracter d'aquell mateix index en la String paraula, a traves de un for.
+     * Despres es crea un int anomenat puntuacioEsperada que es igual a 610 si la String paraula conte 
+     * els caracters: "x", "y" o "h" o a 600 si no conte cap d'aquests caracters. Despres es crea un 
+     * float puntuacioReal que rep el valor que retorna la funcio calcularPuntuacio, que rep per 
+     * parametre la Array de Strings palabraEndevinada i un int 3. Despres comprova si 
+     * puntuacioEsperada es igual a puntuacioReal.
+     */
     @Test
     public void testCalcularPuntuacio5() {
         String paraula=puntuacio.getParaulaSecretaDificultat(3);
@@ -138,8 +216,18 @@ public class PuntuacioTest {
         float puntuacioReal = puntuacio.calcularPuntuacio(palabraEndevinada,3);
         assertEquals(puntuacioEsperada,puntuacioReal);
     }
-
-
+    /** 
+     * Metode test que prova quants de punts es consegueixen amb una praulaEndevinada concreta.
+     * Primer crea una String paraula que rep el valor de la funcio getParaulaSecretaDificultat
+     * pasantli per parametre un int 3. Crea una Array de Strings palabraEndevinada amb una
+     * llaragaria igual a la String paraula creada antes, despres assigna a cada index de 
+     * palabraEndevinada el caracter d'aquell mateix index en la String paraula, a traves de un for.
+     * Despres es crea un int anomenat puntuacioEsperada que es igual a 560 si la String paraula conte 
+     * els caracters: "x", "y" o "h" o a 550 si no conte cap d'aquests caracters. Despres es pausa
+     * l'execucio del metode durant 50 segons,una vegada passats els 50 segons es crea un float puntuacioReal 
+     * que rep el valor que retorna la funcio calcularPuntuacio, que rep per parametre la Array de Strings 
+     * palabraEndevinada i un int 3. Despres comprova si puntuacioEsperada es igual a puntuacioReal.
+     */
     @Test
     public void testCalcularPuntuacio6() throws InterruptedException {
         String paraula=puntuacio.getParaulaSecretaDificultat(3);
@@ -158,7 +246,18 @@ public class PuntuacioTest {
         float puntuacioReal = puntuacio.calcularPuntuacio(palabraEndevinada,3);
         assertEquals(puntuacioEsperada,puntuacioReal);
     }
-
+    /** 
+     * Metode test que prova quants de punts es consegueixen amb una praulaEndevinada concreta.
+     * Primer crea una String paraula que rep el valor de la funcio getParaulaSecretaDificultat
+     * pasantli per parametre un int 2. Crea una Array de Strings palabraEndevinada amb una
+     * llaragaria igual a la String paraula creada antes, despres assigna a cada index de 
+     * palabraEndevinada el caracter d'aquell mateix index en la String paraula, a traves de un for.
+     * Despres es crea un int anomenat puntuacioEsperada que es igual a 350 si la String paraula conte 
+     * els caracters: "x", "y" o "h" o a 540 si no conte cap d'aquests caracters. Despres es pausa
+     * l'execucio del metode durant 60 segons,una vegada passats els 60 segons es crea un float puntuacioReal 
+     * que rep el valor que retorna la funcio calcularPuntuacio, que rep per parametre la Array de Strings 
+     * palabraEndevinada i un int 3. Despres comprova si puntuacioEsperada es igual a puntuacioReal.
+     */
     @Test
     public void testCalcularPuntuacio7() throws InterruptedException {
         String paraula=puntuacio.getParaulaSecretaDificultat(2);
--- src/test/java/com/cifpfbmoll/penjat3/TaulerTest.java
+++ src/test/java/com/cifpfbmoll/penjat3/TaulerTest.java
@@ -6,53 +6,85 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaulerTest {
-
+    /**
+     * Objecte de la clase Tauler per se inicialitzat abans de cada clase.
+     */
     private Tauler tauler;
-
+    /**
+     * Metode que inicialitza l'objecte tauler y despres crida el metode inicialitzarPartida 
+     * damunt de la instancia d'aquest tauler. Pasa per parametre la String "paraula" i l'int 4.
+     * Aquest metode es crida 1 vegada antes de provar cada test.
+     */
     @BeforeEach
     void reiniciar(){
         this.tauler = new Tauler();
         this.tauler.inicialitzarPartida("paraula", 4);
     }
-
+    /**
+     * Metode test que comprova si l'atribut paraulaSecreta de l'objecte tauler es igual
+     * a una Array de caracters concreta.
+     */
     @Test
     void inicialitzarPartidaParaulaSecreta() {
         assertArrayEquals(new char[]{'p', 'a', 'r', 'a', 'u', 'l', 'a'}, this.tauler.getParaulaSecreta());
     }
-
+    /**
+     * Metode test que comprova si l'atribut intents de l'objecte tauler es igual a 4.
+     */
     @Test
     void inicialitzarPartidaNombreIntents() {
         assertEquals(4, this.tauler.getIntents());
     }
-
+    /**
+     * Metode test que comprova si el metode verificar quan li pases una String "ll"
+     * retorna "Lletra incorrecte".
+     */
     @Test
     void verificarEntradaIncorrecte() {
         assertEquals("Lletra incorrecte",this.tauler.verificar("ll"));
     }
-
+    /**
+     * Metode test que primer crida al metode verificar pasant-li per parametre una "a"
+     * i despres comprova si l'atribut palabraEndevinada del objecte tauler es igual
+     * a una Array de Sting concreta.
+     */
     @Test
     void verificarEntradaCorrecteEncertat() {
         this.tauler.verificar("a");
         assertArrayEquals(new String[]{ null,"a",null,"a",null,null,"a" },this.tauler.getPalabraEndevinada());
     }
-
+    /**
+     * Metode test que primer crida al metode verificar pasant-li per parametre una "n"
+     * i despres comprova si l'atribut intents es igual a l'int 3.
+     */
     @Test
     void verificarEntradaCorrecteErrada() {
         this.tauler.verificar("n");
         assertEquals(3,this.tauler.getIntents());
     }
-
+    /**
+     * Metode test que comprova si el que retorna el metode imprimir es igual
+     * a una String concreta.
+     */
     @Test
     void imprimirCapEncert() {
         assertEquals("_______", this.tauler.imprimir());
     }
-
+    /**
+     * Metode test que primer crida al metode verificar pasant-li una "a" per
+     * parametere i despres comprova si el metode imprimir retorna una String
+     * concreta.
+     */
     @Test
     void imprimirAmbLletres() {
         this.tauler.verificar("a");
         assertEquals("_a_a__a", this.tauler.imprimir());
     }
-
+    /**
+     * Metode test que primer va cridant al metode verificar 5 vegades pasant-li 
+     * una "a", una "p", una "r", una "u" i una "l" per parametere i despres 
+     * comprova si el metode imprimir retorna una String en concret.
+     */
     @Test
     void imprimirTotEncertat() {
         this.tauler.verificar("a");
@@ -62,14 +94,18 @@ class TaulerTest {
         this.tauler.verificar("l");
         assertEquals("paraula", this.tauler.imprimir());
     }
-
-
-
+    /**
+     * Metode test que comprova si el metode imprimirVides retorna una String en concret.
+     */
     @Test
     void imprimirVidesPlural() {
         assertEquals("Et queden 4 vides de 4", this.tauler.imprimirVides());
     }
-
+    /**
+     * Metode test que primer va cridant al metode verificar 3 vegades pasant-li 
+     * una "n" 3 vegades per parametere i despres comprova si el metode imprimirVides 
+     * retorna una String en concret.
+     */
     @Test
     void imprimirVidesSingular() {
         this.tauler.verificar("n");
@@ -77,13 +113,21 @@ class TaulerTest {
         this.tauler.verificar("n");
         assertEquals("Et queda 1 vida de 4", this.tauler.imprimirVides());
     }
-
+    /**
+     * Metode test que primer crida al metode verificar pasant-li una "n" per 
+     * parametere i despres comprova si l'atribut intents de l'objecte tauler
+     * te una valor igual a 3.
+     */
     @Test
     void restarIntents() {
         this.tauler.verificar("n");
         assertEquals(3, this.tauler.getIntents());
     }
-
+    /**
+     * Metode test que primer va cridant al metode verificar 5 vegades pasant-li 
+     * una "a", una "p", una "r", una "u" i una "l" per parametere i despres 
+     * comprova si el metode hasGuanyat retorna un boolea true.
+     */
     @Test
     void hasGuanyatTrue() {
         this.tauler.verificar("a");
@@ -93,7 +137,9 @@ class TaulerTest {
         this.tauler.verificar("l");
         assertTrue(this.tauler.hasGuanyat());
     }
-
+    /**
+     * Metode test que comprova si el metode hasGuanyat retorna un boolea false.
+     */
     @Test
     void hasGuanyatFalse() {
         assertFalse(this.tauler.hasGuanyat());
